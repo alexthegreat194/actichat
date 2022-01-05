70443c2ca2b9cfc02e599730507ec84a70aa91a6
alexthegreat194 <alexander.gentry@aol.com>
2022-01-04 19:45:35 -0800

    got nav and basic sockets

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, url_for
+from flask_socketio import SocketIO, send
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret ;)'
+
+socketio = SocketIO(app)
+
+@socketio.on('hello')
+def handle_connect(data):
+    print('received json: ' + str(data))
+
+@socketio.on('message')
+def handle_message(data):
+    print('received json: ' + str(data))
+    send(data)
 
 @app.route('/', methods=['GET'])
 def index():
